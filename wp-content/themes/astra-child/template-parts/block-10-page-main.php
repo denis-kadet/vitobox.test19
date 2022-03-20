02b0f06dc9aee34214cb86a485b690c4b5492670
Denis Vykhodtsev <denis-kadet@mail.ru>
2022-03-20 22:30:06 +0300

    14109 - 20.03.2022

---
+++ wp-content/themes/astra-child/template-parts/block-10-page-main.php
@@ -0,0 +1,245 @@
+<section class="section">
+    <div class="block-10">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="block__title block-10__title">Признаки дефицита витаминов</div>
+                </div>
+                <div class="col-lg-5 col-md-6 d-none d-md-block">
+                    <ul class="block-10__list-items">
+                        <li class="block-10__list-item block-10__active ">Проблемы с кожей</li>
+                        <li class="block-10__list-item ">Выпадение волос</li>
+                        <li class="block-10__list-item ">Длительная усталость</li>
+                        <li class="block-10__list-item ">Проблемы с иммунитетом</li>
+                        <li class="block-10__list-item ">Пигментация</li>
+                        <li class="block-10__list-item ">Проблемы с ЖКТ</li>
+                        <li class="block-10__list-item ">Набор веса </li>
+                        <li class="block-10__list-item ">ПМС </li>
+                        <li class="block-10__list-item ">Повышенная кровоточивость </li>
+                    </ul>
+                </div>
+                <div class="col-lg-6 offset-lg-1 col-md-6 d-none d-md-block">
+                    <div class="block-10__slider-wrap">
+                        <div class="block-10__slider-item">
+                            <div class="block-10__slider-list">
+                                <picture>
+                                    <source srcset=" <?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img.webp" type="image/webp ">
+                                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img.png " alt="icon:app " class="block-10__list-img ">
+                                </picture>
+                                <div class="block-10__list-title">Проблемы с кожей</div>
+                                <div class="block-10__list-text">
+                                    Причиной может стать дефицит витаминов А (шелушение, дерматиты, растрескивания), С (сухость кожи), некоторых витаминов группы B, цинка, серы
+                                </div>
+                            </div>
+                        </div>
+                        <div class="block-10__slider-item">
+                            <div class="block-10__slider-list">
+                                <picture>
+                                    <source srcset="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-2.webp" type="image/webp ">
+                                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-2.jpg" alt="icon:app" class="block-10__list-img">
+                                </picture>
+                                <div class="block-10__list-title">Выпадение волос</div>
+                                <div class="block-10__list-text">Причиной может стать дефицит витаминов А, С, некоторых витаминов группы В, кальция, йода, цинка, серы </div>
+                            </div>
+                        </div>
+                        <div class="block-10__slider-item">
+                            <div class="block-10__slider-list">
+                                <picture>
+                                    <source srcset=" <?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-3.webp " type="image/webp ">
+                                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-3.jpg" alt="icon:app " class="block-10__list-img ">
+                                </picture>
+                                <div class="block-10__list-title ">Длительная усталость</div>
+                                <div class="block-10__list-text ">Причиной может стать дефицит витаминов D, С, Е, некоторых витаминов группы В, железа, кальция, калия, йода</div>
+                            </div>
+                        </div>
+                        <div class="block-10__slider-item ">
+                            <div class="block-10__slider-list ">
+                                <picture>
+                                    <source srcset=" <?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-4.webp " type="image/webp ">
+                                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-4.jpg" alt="icon:app " class="block-10__list-img ">
+                                </picture>
+                                <div class="block-10__list-title ">Проблемы с иммунитетом</div>
+                                <div class="block-10__list-text ">Причиной может стать дефицит витаминов А, D, Е, С, железа, кальция</div>
+                            </div>
+                        </div>
+                        <div class="block-10__slider-item ">
+                            <div class="block-10__slider-list ">
+                                <picture>
+                                    <source srcset=" <?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-5.webp " type="image/webp ">
+                                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-5.jpg" alt="icon:app " class="block-10__list-img ">
+                                </picture>
+                                <div class="block-10__list-title ">Пигментация</div>
+                                <div class="block-10__list-text ">Причиной может стать дефицит железа</div>
+                            </div>
+                        </div>
+                        <div class="block-10__slider-item ">
+                            <div class="block-10__slider-list ">
+                                <picture>
+                                    <source srcset=" <?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-6.webp " type="image/webp ">
+                                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-6.jpg" alt="icon:app " class="block-10__list-img ">
+                                </picture>
+                                <div class="block-10__list-title ">Проблемы с ЖКТ</div>
+                                <div class="block-10__list-text ">Причиной может стать дефицит калия</div>
+                            </div>
+                        </div>
+                        <div class="block-10__slider-item ">
+                            <div class="block-10__slider-list ">
+                                <picture>
+                                    <source srcset=" <?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-7.webp " type="image/webp ">
+                                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-7.jpg" alt="icon:app " class="block-10__list-img ">
+                                </picture>
+                                <div class="block-10__list-title ">Набор веса</div>
+                                <div class="block-10__list-text ">Причиной может стать дефицит йода</div>
+                            </div>
+                        </div>
+                        <div class="block-10__slider-item ">
+                            <div class="block-10__slider-list ">
+                                <picture>
+                                    <source srcset=" <?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-8.webp " type="image/webp ">
+                                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-8.jpg" alt="icon:app " class="block-10__list-img ">
+                                </picture>
+                                <div class="block-10__list-title ">ПМС</div>
+                                <div class="block-10__list-text ">Причиной может стать дефицит магния</div>
+                            </div>
+                        </div>
+                        <div class="block-10__slider-item ">
+                            <div class="block-10__slider-list ">
+                                <picture>
+                                    <source srcset=" <?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-9.webp " type="image/webp ">
+                                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-9.jpg" alt="icon:app " class="block-10__list-img ">
+                                </picture>
+                                <div class="block-10__list-title ">Повышенная кровоточивость</div>
+                                <div class="block-10__list-text ">Может быть вызвана недостатком витаминов К, С, кальция</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 d-md-none d-block ">
+                    <ul id="block-10__accordion" class="block-10__accordion ">
+                        <!-- Section 1 -->
+                        <li>
+                            <h4 class="block-10__accordion-title ">Проблемы с кожей</h4>
+                            <ul class="block-10__accordion-items ">
+                                <li class="block-10__accordion-item ">
+                                    <picture>
+                                        <source srcset=" <?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img.webp " type="image/webp ">
+                                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img.png " alt="icon:app " class="block-10__list-img ">
+                                    </picture>
+                                </li>
+                                <li class="block-10__accordion-item ">
+                                    Причиной может стать дефицит витаминов А (шелушение, дерматиты, растрескивания), С (сухость кожи), некоторых витаминов группы B, цинка, серы
+                                </li>
+                            </ul>
+                        </li>
+                        <!-- Section 2 -->
+                        <li>
+                            <h3 class="block-10__accordion-title ">Выпадение волос</h3>
+                            <ul class="block-10__accordion-items ">
+                                <li class="block-10__accordion-item ">
+                                    <picture>
+                                        <source srcset=" <?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-2.webp " type="image/webp ">
+                                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-2.jpg" alt="icon:app " class="block-10__list-img ">
+                                    </picture>
+                                </li>
+                                <li class="block-10__accordion-item ">Причиной может стать дефицит витаминов А, С, некоторых витаминов группы В, кальция, йода, цинка, серы </li>
+                            </ul>
+                        </li>
+                        <!-- Section 3 -->
+                        <li>
+                            <h3 class="block-10__accordion-title ">Длительная усталость</h3>
+                            <ul class="block-10__accordion-items ">
+                                <li class="block-10__accordion-item ">
+                                    <picture>
+                                        <source srcset=" <?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-3.webp" type="image/webp ">
+                                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-3.jpg" alt="icon:app " class="block-10__list-img ">
+                                    </picture>
+                                </li>
+                                <li class="block-10__accordion-item ">Причиной может стать дефицит витаминов D, С, Е, некоторых витаминов группы В, железа, кальция, калия, йода</li>
+                            </ul>
+                        </li>
+                        <!-- Section 4 -->
+                        <li>
+                            <h3 class="block-10__accordion-title ">Проблемы с иммунитетом</h3>
+                            <ul class="block-10__accordion-items ">
+                                <li class="block-10__accordion-item ">
+                                    <picture>
+                                        <source srcset=" <?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-4.webp" type="image/webp ">
+                                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-4.jpg" alt="icon:app " class="block-10__list-img ">
+                                    </picture>
+                                </li>
+                                <li class="block-10__accordion-item ">Причиной может стать дефицит витаминов А, D, Е, С, железа, кальция</li>
+                            </ul>
+                        </li>
+                        <!-- Section 5 -->
+                        <li>
+                            <h3 class="block-10__accordion-title ">Пигментация</h3>
+                            <ul class="block-10__accordion-items ">
+                                <li class="block-10__accordion-item ">
+                                    <picture>
+                                        <source srcset=" <?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-5.webp" type="image/webp ">
+                                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-5.jpg" alt="icon:app " class="block-10__list-img ">
+                                    </picture>
+                                </li>
+                                <li class="block-10__accordion-item ">Причиной может стать дефицит железа</li>
+                            </ul>
+                        </li>
+                        <!-- Section 6 -->
+                        <li>
+                            <h3 class="block-10__accordion-title ">Проблемы с ЖКТ</h3>
+                            <ul class="block-10__accordion-items ">
+                                <li class="block-10__accordion-item ">
+                                    <picture>
+                                        <source srcset=" <?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-6.webp" type="image/webp ">
+                                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-6.jpg" alt="icon:app " class="block-10__list-img ">
+                                    </picture>
+                                </li>
+                                <li class="block-10__accordion-item ">Причиной может стать дефицит калия</li>
+                            </ul>
+                        </li>
+                        <!-- Section 7 -->
+                        <li>
+                            <h3 class="block-10__accordion-title ">Набор веса</h3>
+                            <ul class="block-10__accordion-items ">
+                                <li class="block-10__accordion-item ">
+                                    <picture>
+                                        <source srcset=" <?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-7.webp " type="image/webp ">
+                                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-7.jpg" alt="icon:app " class="block-10__list-img ">
+                                    </picture>
+                                </li>
+                                <li class="block-10__accordion-item ">ЧПричиной может стать дефицит йода</li>
+                            </ul>
+                        </li>
+                        <!-- Section 8 -->
+                        <li>
+                            <h3 class="block-10__accordion-title ">ПМС</h3>
+                            <ul class="block-10__accordion-items ">
+                                <li class="block-10__accordion-item ">
+                                    <picture>
+                                        <source srcset=" <?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-8.webp" type="image/webp ">
+                                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-8.jpg" alt="icon:app " class="block-10__list-img ">
+                                    </picture>
+                                </li>
+                                <li class="block-10__accordion-item ">Причиной может стать дефицит магния</li>
+                            </ul>
+                        </li>
+                        <!-- Section 9 -->
+                        <li>
+                            <h3 class="block-10__accordion-title ">Повышенная кровоточивость</h3>
+                            <ul class="block-10__accordion-items ">
+                                <li class="block-10__accordion-item ">
+                                    <picture>
+                                        <source srcset=" <?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-9.webp " type="image/webp ">
+                                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/block-10-img-9.jpg" alt="icon:app " class="block-10__list-img ">
+                                    </picture>
+                                </li>
+                                <li class="block-10__accordion-item ">Может быть вызвана недостатком витаминов К, С, кальция</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
