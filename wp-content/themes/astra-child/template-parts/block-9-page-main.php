f2ab36cf7f21961aadb78e1e6bd54349fa78d00c
Denis Vykhodtsev <denis-kadet@mail.ru>
2022-03-21 10:34:33 +0300

    14109 - 21.03.2022 carbon  fields

--- wp-content/themes/astra-child/template-parts/block-9-page-main.php
+++ wp-content/themes/astra-child/template-parts/block-9-page-main.php
@@ -1,77 +1,31 @@
+<?php
+$support = carbon_get_theme_option('crb_places');
+$check__support_1 = carbon_get_theme_option('crb_show_content-1');
+$title_support_1 = carbon_get_theme_option('crb_show_content-title-1');
+?>
 <section class="section">
     <div class="block-9">
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <div class="block__title block-9__title">Частые вопросы</div>
+                    <div class="block__title block-9__title"><?= esc_html( $title_support_1) ?></div>
                 </div>
                 <div class="col-lg-7 offset-lg-3">
                     <div class="block-9__bg"></div>
                     <ul id="block-9__accordion" class="block-9__accordion">
-                        <!-- Section 1 -->
-                        <li class="block-9__accordion-inner">
-                            <h3 class="block-9__accordion-title"><span>Персонализированный набор лучше обычного витаминного?</span></h3>
-                            <ul class="block-9__accordion-items">
-                                <li class="block-9__accordion-item ">Жители России принимают витамины, но 92% делают это бессистемно, без учёта потребностей организма, ориентируясь только на отзывы знакомых и рекламу. Наш тест учитывает образ жизни, цели и самочувствие, а также сочетаемость
-                                    и дополняемость компонентов.</li>
-                            </ul>
-                        </li>
-                        <!-- Section 2 -->
-                        <li class="block-9__accordion-inner">
-                            <h3 class="block-9__accordion-title"><span>Где вы производите витамины?</span></h3>
-                            <ul class="block-9__accordion-items">
-                                <li class="block-9__accordion-item"><span>Продукцию для наших наборов производит компания GPC Group, которая много лет специализируется на производстве витаминов, минералов и спортивного питания. Все сертификаты качества и соответствий мы
-                                            можем предоставить вам по запросу.</span></li>
-                            </ul>
-                        </li>
-                        <!-- Section 3 -->
-                        <li class="block-9__accordion-inner">
-                            <h3 class="block-9__accordion-title"><span>Что делать, когда набор закончится?</span></h3>
-                            <ul class="block-9__accordion-items">
-                                <li class="block-9__accordion-item"><span>Вам придёт письмо с подробной инструкцией и ссылкой на тест для уточнения деталей.</span></li>
-                            </ul>
-                        </li>
-                        <!-- Section 4 -->
-                        <li class="block-9__accordion-inner">
-                            <h3 class="block-9__accordion-title "><span>Почему я могу доверять вашему тесту?</span></h3>
-                            <ul class="block-9__accordion-items ">
-                                <li class="block-9__accordion-item "><span>Умный алгоритм VitoBox разработан командой профессиональных врачей, нутрициологов и IT-специалистов. Его вопросы схожи с теми, что задают врачи на очных консультациях, и позволяют нам получить всю необходимую информацию
-                                            для точного подбора витаминов.</span></li>
-                                <li class="block-9__accordion-item "><span>Тест был проверен более 50 раз разными специалистами, рекомендации алгоритма совпадают более чем на 90% с рекомендациями врачей-эндокринологов, которые были ими даны в ходе личного обследования и теста на кровь.</span></li>
-                                <li class="block-9__accordion-item "><span>Алгоритм регулярно дорабатывается на основе опыта клиентов, что делает точность подбора ещё выше.</span></li>
-                            </ul>
-                        </li>
-                        <!-- Section 5 -->
-                        <li class="block-9__accordion-inner">
-                            <h3 class="block-9__accordion-title"><span>Как мне доставят набор?</span></h3>
-                            <ul class="block-9__accordion-items">
-                                <li class="block-9__accordion-item"><span>Доставка работает по всей России. В крупные города набор придёт за 2−3 дня, в регионы — за 4−5. Мы отправим вам ссылку на сайт, где ваш заказ можно будет отследить.</span></li>
-                                <li class="block-9__accordion-item"><span>Стоимость доставки рассчитывается при оформлении заказа, средняя стоимость — 400 рублей.</span></li>
-                                <li class="block-9__accordion-item"><span>В страны СНГ мы пока не доставляем, но работаем над этим.</span></li>
-                            </ul>
-                        </li>
-                        <!-- Section 6 -->
-                        <li class="block-9__accordion-inner">
-                            <h3 class="block-9__accordion-title "><span>Где я могу посмотреть состав своего набора?</span> </h3>
-                            <ul class="block-9__accordion-items ">
-                                <li class="block-9__accordion-item"><span>После прохождения теста вам придёт письмо с составом набора.</span></li>
-                                <li class="block-9__accordion-item"><span>Каждый следующий месяц мы будем рекомендовать вам новый набор витаминов с учётом предыдущего опыта и специального дополнительного опроса для оценки вашего прогресса.</span></li>
-                            </ul>
-                        </li>
-                        <li class="block-9__accordion-inner">
-                            <h3 class="block-9__accordion-title"><span>Я могу получить скидку на набор?</span></h3>
-                            <ul class="block-9__accordion-items">
-                                <li class="block-9__accordion-item"><span>Чтобы получить скидку на покупку, достаточно сделать сторис или пост в Instagram про наши витамины, добавив отметку @vitobox.ru. За отметку мы дарим промокод на скидку 800 рублей, который вы получите в личные сообщения
-                                            в Instagram.</span></li>
-                                <li class="block-9__accordion-item"><span>Также вы можете подписаться на наши социальные сети и следить за акциями.</span></li>
-                            </ul>
-                        </li>
-                        <li class="block-9__accordion-inner">
-                            <h3 class="block-9__accordion-title"><span>У ваших витаминов есть противопоказания?</span></h3>
-                            <ul class="block-9__accordion-items">
-                                <li class="block-9__accordion-item"><span>Наши витамины не имеют противопоказаний, кроме случаев индивидуальной реакции на компоненты и ряда хронических заболеваний. Вы можете получить консультацию, позвонив нам или написав на нашу электронную почту.</span></li>
-                            </ul>
-                        </li>
+
+
+                        <?php foreach ($support as $items): ?>
+                            <li class="block-9__accordion-inner">
+                                <h3 class="block-9__accordion-title"><span><?= esc_html($items['support__text-faq']) ?></span></h3>
+                                <ul class="block-9__accordion-items">
+                                    <?php foreach ($items['support__text-faq_sub'] as $item): ?>
+                                        <li class="block-9__accordion-item"><?= esc_html($item['support__fragment-text']) ?></li>
+                                    <?php endforeach; ?>
+                                </ul>
+                            </li>
+                        <?php endforeach; ?>
+
                     </ul>
                 </div>
 
