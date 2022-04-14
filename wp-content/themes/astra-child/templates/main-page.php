0ed0abfd2a17636fc3a379a4fd2e6016f3c9d472
Denis Vykhodtsev <denis-kadet@mail.ru>
2022-04-14 15:21:51 +0300

    14109 - 14.04.2022

--- wp-content/themes/astra-child/templates/main-page.php
+++ wp-content/themes/astra-child/templates/main-page.php
@@ -8,7 +8,7 @@ get_template_part('template-parts/block-12-page-main');
 get_template_part('template-parts/block-1-page-main');
 get_template_part('template-parts/block-2-page-main');
 get_template_part('template-parts/block-3-page-main');
-get_template_part('template-parts/block-4-page-main');
+//get_template_part('template-parts/block-4-page-main');
 get_template_part('template-parts/block-5-page-main');
 get_template_part('template-parts/block-6-page-main');
 get_template_part('template-parts/block-7-page-main');
